bff9abe98e7d0f347973485283935aa949e631b1
Chantal Sorias <chantal_sorias@hotmail.com>
2024-09-16 15:05:52 -0400

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Chantal on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
